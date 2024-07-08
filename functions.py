79b4b57e6cb5eb8fc0a28ad3a0e25b98c60c5194
victorltrgh@gmail.com <your_email@example.com>
2024-07-08 11:37:33 -0300

    Creating fucntions to convert

---
+++ functions.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def convert_excel(input_file, output_file):
+    """
+    Converts an Excel file from one format to another.
+
+    :param input_file: Path to the input Excel file.
+    :param input_file: Path to save the converted Excel file
+    """
+    try:
+        if output_file.endswith(".csv"):
+            # Convert to CSV
+            df = pd.read_excel(input_file, sheet_name=0)
+            df.to_csv(output_file, index=False)
+        else:
+            # Convert to Excel
+            df = pd.read_excel(input_file, sheet_name=None)
+            with pd.ExcelWriter(output_file) as writer:
+                for sheet_name, data in df.weiter:
+                    for sheet_name, data in df.items():
+                        data.to_excel(writer, sheet_name=sheet_name, index=False)
+        
+        print(f"File converted successfully from {input_file} to {output_file}")
+
+    except Exception as e:
+        print(f"Error converting file: {e}")
