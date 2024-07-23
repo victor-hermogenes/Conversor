9d516aa83c0dec075505a2814db5642bdb73ed2b
victor-hermogenes <victorltrgh@gmail.com>
2024-07-23 14:06:27 -0300

    Adding a delete original sheet when finish to convert and fragmentate.

--- functions.py
+++ functions.py
@@ -2,6 +2,10 @@ import pandas as pd
 import json
 import math
 import os
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def save_fragmented_csv(df, output_file, fragment_size_mb):
     rows_per_fragment = math.ceil(fragment_size_mb * 1024 * 1024 / df.memory_usage(index=True, deep=True).sum() * len(df))
@@ -12,7 +16,7 @@ def save_fragmented_csv(df, output_file, fragment_size_mb):
         fragment_df = df.iloc[i * rows_per_fragment:(i + 1) * rows_per_fragment]
         fragment_file = f"{base_name}_part{i+1}{ext}"
         fragment_df.to_csv(fragment_file, index=False)
-        print(f"Saved fragment {i+1} to {fragment_file}")
+        logging.info(f"Saved fragment {i+1} to {fragment_file}")
 
 def save_fragmented_excel(df, output_file, fragment_size_mb):
     rows_per_fragment = math.ceil(fragment_size_mb * 1024 * 1024 / df.memory_usage(index=True, deep=True).sum() * len(df))
@@ -23,7 +27,7 @@ def save_fragmented_excel(df, output_file, fragment_size_mb):
         fragment_df = df.iloc[i * rows_per_fragment:(i + 1) * rows_per_fragment]
         fragment_file = f"{base_name}_part{i+1}{ext}"
         fragment_df.to_excel(fragment_file, index=False)
-        print(f"Saved fragment {i+1} to {fragment_file}")
+        logging.info(f"Saved fragment {i+1} to {fragment_file}")
 
 def convert_json_to_csv(input_file, output_file, selected_columns):
     try:
@@ -36,9 +40,9 @@ def convert_json_to_csv(input_file, output_file, selected_columns):
         df = df[selected_columns]
 
         df.to_csv(output_file, index=False, encoding='utf-8')
-        print(f"File converted successfully from {input_file} to {output_file}")
+        logging.info(f"File converted successfully from {input_file} to {output_file}")
     except Exception as e:
-        print(f"Error converting file: {e}")
+        logging.error(f"Error converting file: {e}")
 
 def convert_csv_to_excel(input_file, output_file, selected_columns):
     try:
@@ -46,9 +50,9 @@ def convert_csv_to_excel(input_file, output_file, selected_columns):
         df = df[selected_columns]
 
         df.to_excel(output_file, index=False)
-        print(f"File converted successfully from {input_file} to {output_file}")
+        logging.info(f"File converted successfully from {input_file} to {output_file}")
     except Exception as e:
-        print(f"Error converting file: {e}")
+        logging.error(f"Error converting file: {e}")
 
 def convert_excel(input_file, output_file, selected_columns):
     try:
@@ -59,9 +63,9 @@ def convert_excel(input_file, output_file, selected_columns):
             df.to_csv(output_file, index=False, encoding='utf-8')
         else:
             df.to_excel(output_file, index=False)
-        print(f"File converted successfully from {input_file} to {output_file}")
+        logging.info(f"File converted successfully from {input_file} to {output_file}")
     except Exception as e:
-        print(f"Error converting file: {e}")
+        logging.error(f"Error converting file: {e}")
 
 def fragment_file(output_file, fragment_size_mb):
     try:
@@ -71,6 +75,14 @@ def fragment_file(output_file, fragment_size_mb):
         elif output_file.endswith('.xlsx'):
             df = pd.read_excel(output_file)
             save_fragmented_excel(df, output_file, fragment_size_mb)
-        print(f"File fragmented successfully from {output_file}")
+        logging.info(f"File fragmented successfully from {output_file}")
+        delete_original_file(output_file)
     except Exception as e:
-        print(f"Error fragmenting file: {e}")
+        logging.error(f"Error fragmenting file: {e}")
+
+def delete_original_file(file_path):
+    try:
+        os.remove(file_path)
+        logging.info(f"Deleted original file: {file_path}")
+    except Exception as e:
+        logging.error(f"Error deleting file {file_path}: {e}")
